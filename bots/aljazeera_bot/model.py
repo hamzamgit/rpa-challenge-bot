51cf14892e74482ad263482f126ad4ca8b108702
Hamza Mushtaq <hamza1mushtaq@gmail.com>
2024-07-20 04:23:04 +0500

    FEAT: improved load_items function, default values, added get_headers function

--- bots/aljazeera_bot/model.py
+++ bots/aljazeera_bot/model.py
@@ -4,18 +4,17 @@ from models import BaseItem
 
 class AljazeeraModel(BaseItem):
 
-    def count_search_phrases(self, search_phrase):
-        search_phrases = re.findall(search_phrase, f"{self.title} {self.description}", re.IGNORECASE)
+    @property
+    def count_search_phrases(self) -> int:
+        search_phrases = re.findall(self.search_phrase, f"{self.title} {self.description}", re.IGNORECASE)
         return len(search_phrases)
 
     @property
-    def contains_money(self):
-        money_patterns = [
-            r"\$\d+(\.\d{1,2})?",
-            r"\d+\s+dollars?",
+    def contains_money(self) -> bool:
+        money_pattern = re.compile(
+            r"\$\d+(\.\d{1,2})?|"
+            r"\d+\s+dollars?|"
             r"\d+\s+USD"
-        ]
-        for pattern in money_patterns:
-            if re.search(pattern, f"{self.title} {self.description}"):
-                return True
-        return False
+        )
+        return bool(money_pattern.search(f"{self.title} {self.description}"))
+
--- models/base.py
+++ models/base.py
@@ -1,6 +1,7 @@
+import inspect
 import re
-from dataclasses import dataclass
-from typing import Iterable
+from dataclasses import dataclass, fields
+from typing import Iterable, Dict, Any
 from datetime import datetime
 
 
@@ -9,18 +10,34 @@ class BaseItem:
     title:          str
     description:    str
     image_path:     str
+    search_phrase:  str
     publish_date:   datetime
-    article_url:    str = ''
-    scrap_date:     str = datetime.now()
+    created_at:     datetime = datetime.now()
 
     def add_value(self, name, value):
         if hasattr(self, name):
             setattr(self, name, value)
         raise AttributeError(f"{name:} not specified")
 
-    def load_items(self):
-        return {item: getattr(self, item) for item in dir(self)}
+    def get_headers(self):
+        return list(self.load_items().keys())
 
-    def __dir__(self) -> Iterable[str]:
-        dirs = super(BaseItem, self).__dir__()
-        return [attr for attr in dirs if '__' not in attr]
+    def load_items(self) -> Dict[str, Any]:
+        """
+        Get all fields and properties of the dataclass along with their values.
+
+        Returns:
+            dict: A dictionary with attribute names as keys and their corresponding values.
+        """
+
+        # Get all dataclass fields
+        dataclass_fields = {f.name: getattr(self, f.name) for f in fields(self)}
+
+        # Get all properties
+        properties = {
+            name: getattr(self, name)
+            for name, value in inspect.getmembers(self.__class__, lambda v: isinstance(v, property))
+        }
+
+        # Combine both
+        return {**dataclass_fields, **properties}
